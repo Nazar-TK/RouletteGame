7e099cfe2ba518fb3bd53891d814cfce05402536
Nazar Kalapun <ntkalapun@gmail.com>
2024-06-09 12:51:30 +0300

    Created splash screen for application.

--- app/src/main/java/com/example/roulettegame/MainActivity.kt
+++ app/src/main/java/com/example/roulettegame/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.roulettegame
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.view.View
+import android.view.animation.OvershootInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,11 +13,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.roulettegame.ui.theme.RouletteGameTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
+
         setContent {
             RouletteGameTheme {
                 // A surface container using the 'background' color from the theme
